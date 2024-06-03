deb9302cb6c45130b9b1ca22b057dff432ce4554
Ian Baron <ianezebaron@gmail.com>
2024-06-03 09:02:51 -0300

    menos de 4 ruedas setactive false

---
+++ Assets/VehiculosManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehiculosManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vehiculo vehiculoScript;
+    
+    
+    void Start()
+    {
+        int numRuedas = vehiculoScript.cantRuedas;
+        Debug.Log("cantidad ruedas: " + numRuedas);
+
+        if (numRuedas < 6)
+        {
+            vehiculoScript.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
